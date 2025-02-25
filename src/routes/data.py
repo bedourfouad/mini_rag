ce55f3763a1a2d55856f11abf727b0b80043868b
bedourfouad <bedour9876@gmail.com>
2025-02-25 23:09:43 +0200

    update instruction

--- src/routes/data.py
+++ src/routes/data.py
@@ -8,8 +8,7 @@ from models import ResponseSignal
 import logging
 from .schemes.data import ProcessRequest
 from models.ProjectModel import ProjectModel
-from models.ChunkModel import ChunkModel
-from models.db_schemes import DataChunk
+
 
 logger = logging.getLogger('uvicorn.error')
 
