e9168d3ec232fc4b8694e5aa5c444f24cd79cae4
bedourfouad <bedour9876@gmail.com>
2025-03-02 22:30:18 +0200

    update instruction

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -7,13 +7,13 @@ class ProjectModel(BaseDataModel):
     def __init__(self, db_client: object):
         super().__init__(db_client=db_client)
         self.collection = self.db_client[DataBaseEnum.COLLECTION_PROJECT_NAME.value]
-        
+
     @classmethod
     async def create_instance(cls, db_client: object):
         instance = cls(db_client)
         await instance.init_collection()
         return instance
-    
+
     async def init_collection(self):
         all_collections = await self.db_client.list_collection_names()
         if DataBaseEnum.COLLECTION_PROJECT_NAME.value not in all_collections:
@@ -26,10 +26,11 @@ class ProjectModel(BaseDataModel):
                     unique=index["unique"]
                 )
 
+
     async def create_project(self, project: Project):
 
         result = await self.collection.insert_one(project.dict(by_alias=True, exclude_unset=True))
-        project._id = result.inserted_id
+        project.id = result.inserted_id
 
         return project
 
@@ -64,5 +65,6 @@ class ProjectModel(BaseDataModel):
             projects.append(
                 Project(**document)
             )
+
 
         return projects, total_pages
